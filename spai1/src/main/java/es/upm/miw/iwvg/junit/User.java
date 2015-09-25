555eedea201c4489eaa5a0a130db41d9177d18c8
https://github.com/MiriamSP/GIT.git <miriam.sanz.pascua@alumnos.upm.es>
2015-09-25 20:45:09 +0200

    user.java change

--- spai1/src/main/java/es/upm/miw/iwvg/junit/User.java
+++ spai1/src/main/java/es/upm/miw/iwvg/junit/User.java
@@ -24,7 +24,7 @@ public class User {
     }
 
     public String initials() {
-        return this.name.substring(0, 1) + ".";
+        return this.name.substring(0, 1) + "-";
     }
 
     public int getNumber() {
